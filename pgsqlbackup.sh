515cc2e0aece34fde8297ea54a42997a2e68a269
Chrononaut <bjorn.maeland@gmail.com>
2008-07-16 22:23:58 +0200

    Moved configuration into a separate file

--- pgsqlbackup.sh
+++ pgsqlbackup.sh
@@ -3,28 +3,11 @@
 # PostgreSQL backup script, by Chrononaut <bjorn.maeland@gmail.com>
 # Keeping it simple for now
 
-USER="bjorn"
-
-# Root dir for backups - don't store other files here as they could get deleted by the script.
-BACKUPDIR="/Users/bjorn/pgsql_backups"
-
-# Directory to put the backup files
-DIR=`date +%d-%m-%Y`
-
-# Number of copies to keep
-KEEP=14
-
-# Which compressor to use (gzip/bzip2)
-COMPRESSOR="bzip2"
-
-# Since each element in this list is passed to an inverted grep, you can exclude
-# multiple databases with a single matching word.
-EXCLUDE="test development postgres template"
-
-# Post hook, can be uncommented. Can be used send mails or copy the files offsite, etc.
-#POSTBACKUP="zsh ~/rsync_pgsql_backups"
-
-# -- End of config -- #
+if [ ! -f config ]; then
+  echo "Copy config.sample to config and edit the settings, then run this script again."
+  exit
+fi
+source config
 
 DATABASES=`psql -U $USER -l | grep "^ \w" |  awk -F '|' '{ printf("%s\n", $1) }' | sed 's/ //g'`
 
@@ -32,7 +15,7 @@ for ex in `echo $EXCLUDE`; do
   DATABASES=`echo $DATABASES | grep -v $ex`
 done
 
-if [ ! -d $BACKUPDIR]; then
+if [ ! -d $BACKUPDIR ]; then
   mkdir $BACKUPDIR
 fi
 cd $BACKUPDIR
@@ -53,7 +36,7 @@ while [ `ls|wc -l` -gt $KEEP ]; do
   rm -rf `ls|head -1`
 done
 
-if [ "$POSTBACKUP" ]; then
-  eval $POSTBACKUP
+if [ "$POSTHOOK" ]; then
+  eval $POSTHOOK
 fi
 
