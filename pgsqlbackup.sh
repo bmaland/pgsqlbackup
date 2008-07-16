700cf39dabe797f4918c3f3bee9dd3809292c79c
Chrononaut <bjorn.maeland@gmail.com>
2008-07-17 00:18:50 +0200

    A bunch of improvements

--- pgsqlbackup.sh
+++ pgsqlbackup.sh
@@ -1,32 +1,38 @@
 #!/usr/bin/env zsh
 
-# PostgreSQL backup script, by Chrononaut <bjorn.maeland@gmail.com>
-# Keeping it simple for now
+# PostgreSQL backup script, by Bjørn Arild Mæland <bjorn.maeland at gmail.com>
+# WWW: http://github.com/Chrononaut/pgsqlbackup/tree/master
 
+# Load config
 if [ ! -f config ]; then
   echo "Copy config.sample to config and edit the settings, then run this script again."
   exit
 fi
 source config
 
-DATABASES=`psql -U $USER -l | grep "^ \w" |  awk -F '|' '{ printf("%s\n", $1) }' | sed 's/ //g'`
+# Functions
+create_and_cd() {
+  if [ ! -d $1 ]; then
+    mkdir -p $1
+  fi
+  cd $1
+}
 
-for ex in `echo $EXCLUDE`; do
-  DATABASES=`echo $DATABASES | grep -v $ex`
-done
+# The rest
+if [ "$EXCLUDE" ]; then
+  DATABASES=`psql -U $USER -l | grep "^ \w" |  awk -F '|' '{ printf("%s\n", $1) }' | sed 's/ //g'`
 
-if [ ! -d $BACKUPDIR ]; then
-  mkdir $BACKUPDIR
+  for ex in `echo $EXCLUDE`; do
+    DATABASES=`echo $DATABASES | grep -v $ex`
+  done
 fi
-cd $BACKUPDIR
 
-if [ ! -d $DIR ]; then
-  mkdir $DIR
-fi
-cd $DIR
+create_and_cd $BACKUPDIR
+create_and_cd $DIR
 
 for db in `echo $DATABASES`; do
-  pg_dump -U $USER $db | $COMPRESSOR > $db.db.bz2
+  pg_dump -U $PGUSER -h $PGHOST -F t $db > $db.tar
+  $COMPRESSOR $db.tar
 done
 
 cd ..
